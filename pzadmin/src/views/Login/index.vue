51b1ea82e71112f4076c313eb1a70c2cbdcee90b
zuimoliyan <2019445028@qq.com>
2025-07-11 10:02:28 +0800

    增加网站根目录页面的布局，对每个布局页面进行封装，在components文件夹下创建aside.vue

--- pzadmin/src/views/Login/index.vue
+++ pzadmin/src/views/Login/index.vue
@@ -2,12 +2,6 @@
     <!--登录界面-->
     <div>
         Login<br/>
-    <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
     </div>
 </template>
 
--- pzadmin/src/views/Main.vue
+++ pzadmin/src/views/Main.vue
@@ -1,9 +1,19 @@
 <template>
-    <!--主体界面-->
-    <div>
-        Layout
-    </div>
+  <div class="common-layout">
+    
+      <el-container>
+        <Aside />
+        <!-- <el-aside width="200px">Aside</el-aside> -->
+    <el-container>
+
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+      
+    </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script>
+import Aside from "../components/aside.vue";
 </script>
