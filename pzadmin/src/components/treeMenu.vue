6b3d34666931c43bb7248ce08b791da946c2e6cc
zuimoliyan <2019445028@qq.com>
2025-07-14 09:07:55 +0800

    为菜单点击设置跳转事件，添加markdown文档更新

--- pzadmin/src/components/treeMenu.vue
+++ pzadmin/src/components/treeMenu.vue
@@ -3,7 +3,9 @@
     <template v-for="(item, index) in props.menuData">
 
         <!--这里是没有子菜单的情况-->
+        <!--当点击菜单树中的按钮时，传递当前数据、索引-->
         <el-menu-item
+        @click="handleClick(item,`${props.index}-${item.meta.id}`)"
         v-if="!item.children || item.children.length == 0"
         :index="`${props.index}-${item.meta.id}`"
         :key="`${props.index}-${item.meta.id}`">
@@ -53,9 +55,27 @@
 </template>
 
 <script setup>
+
+import { useRouter } from "vue-router";
+
 //defineProps 是一个编译时特性,语法糖
 //如果不在 <script setup> 中使用，Vue 无法在编译时正确解析它
+
+//创建路由实例
+const router = useRouter()
+
+//点击菜单跳转
 const props = defineProps(['menuData', 'index'])
+
+//这里有一个问题：
+// 为什么这里明明没有在二级菜单上定义点击事件，却还是能跳转？
+//答：即使二级菜单没有直接绑定点击事件，点击事件也会冒泡到一级菜单 （事件冒泡机制（Event Bubbling））
+const handleClick = (item,active) =>{
+    //根据 item.meta.path 的值，导航到指定的路由路径
+    //如果 item.meta.path 是 /home，那么这段代码就会将页面导航到 /home 路由对应的组件
+    router.push(item.meta.path)
+}
+
 console.log(props);
 
 </script>
