e2c4ab26f8b5a140dbd0c3cb928c6d6eabf0ad73
zuimoliyan <2019445028@qq.com>
2025-07-22 09:40:28 +0800

    添加表单账号提交的具体代码实现 、修正一处校验错误，添加请求权限账号列表

--- pzadmin/src/api/index.js
+++ pzadmin/src/api/index.js
@@ -2,25 +2,35 @@ import request from "../utils/request";
 
 //发送验证码
 export const getCode = (data) => {
-    return request.post('/get/code',data)
+    return request.post('/get/code', data)
 }
 
 //注册校验验证码
-export const userAuthentication = (data)=>{
-    return request.post('/user/authentication',data)
+export const userAuthentication = (data) => {
+    return request.post('/user/authentication', data)
 }
 
 //登录页面
-export const login = (data)=>{
-    return request.post('/login',data)
+export const login = (data) => {
+    return request.post('/login', data)
 }
 
 //权限管理列表
-export const authAdmin = (params)=>{
-    return request.get('/menu/list',{params})
+export const authAdmin = (params) => {
+    return request.get('/menu/list', { params })
 }
 
 //菜单权限获取
-export const userGetMenu = ()=>{
+export const userGetMenu = () => {
     return request.get('/user/getmenu')
+}
+
+//菜单权限修改
+export const userSetMenu = (data) => {
+    return request.post('/user/setmenu', data)
+}
+
+//菜单权限列表
+export const menuList = (params) => {
+    return request.get('/menu/list', { params })
 }
--- pzadmin/src/views/auth/group/index.vue
+++ pzadmin/src/views/auth/group/index.vue
@@ -2,13 +2,16 @@
     <button @click="dialogFormVisable = true">添加</button>
     <el-dialog v-model="dialogFormVisable" :before-close="beforeClose" title="添加权限" width="500">
         <el-form ref="formRef" label-width="100px" label-position="left" :model="form" :rules="rules">
+            <el-form-item v-show="false" prop="id">
+                <el-input v-model="form.id" />
+            </el-form-item>
             <el-form-item label="名称" prop="name">
-                <el-input v-model="form.name" placeholder="请填写权限名称" :autofocus="true"/>
+                <el-input v-model="form.name" placeholder="请填写权限名称" :autofocus="true" />
             </el-form-item>
 
             <el-form-item label="权限" prop="permissions">
                 <el-tree ref="treeRef" :data="permissionData" style="max-width: 600px;" node-key="id" show-checkbox
-                    :default-checked-keys="defaultKeys" :default-expanded-keys="[2]"/>
+                    :default-checked-keys="defaultKeys" :default-expanded-keys="[2]" />
             </el-form-item>
         </el-form>
 
@@ -22,18 +25,21 @@
 
 <script setup>
 import { ref, reactive, onMounted } from 'vue';
-import { userGetMenu } from '../../../api';
+import { userGetMenu, userSetMenu, menuList } from '../../../api';
 
-// 关闭弹窗的回调
-const beforeClose = () => {
-    dialogFormVisable.value = false;
-};
 
 // 选中权限
 const defaultKeys = ref([4, 5]);
 
 // 树形结构菜单权限数据
 const permissionData = ref([]);
+
+// 控制弹窗的显示
+const dialogFormVisable = ref(false);
+
+const treeRef = ref()
+const formRef = ref()
+
 
 // 获取菜单权限数据
 // 典中典一个bug改一天 => 
@@ -51,16 +57,29 @@ onMounted(() => {
 })
 
 
+// 关闭弹窗的回调
+const beforeClose = () => {
+    dialogFormVisable.value = false;
+};
+
 // form 数据
 const form = reactive({
+    id: '',
     name: '',
     permissions: ''
 });
 
-// 控制弹窗的显示
-const dialogFormVisable = ref(false);
+const paginationData = reactive({
+    pageNum: 1,
+    pageSize: 10
+})
 
-const treeRef = ref()
+//请求列表数据
+const geiListData = () => {
+    menuList(paginationData).then(({ data }) => {
+
+    })
+}
 
 const rules = reactive({
     name: [
@@ -72,16 +91,25 @@ const rules = reactive({
 //表单提交
 const confirm = async (formEl) => {
 
+    console.log(formEl.value)
     if (!formEl) return
+
+    console.log("group/index.vue.confirm：判断formEl不为空");
 
     await formEl.validate((valid, fields) => {
         if (valid) {
-
+            console.log("group/index.vue.confirm.formEl.validate：表单校验通过，不为空");
+            //获取选择的check实例
+            const permissions = JSON.stringify(treeRef.value.getCheckedKeys())
+            //表单提交
+            userSetMenu({ name: form.name, permissions, id: form.id }).then(({ data }) => {
+                console.log(data);
+            })
         } else {
+            console.log("group/index.vue.confirm.formEl.validate：表单校验不通过，为空");
             console.log('error submit!', fields)
         }
     })
-
 }
 
 </script>
