b65bb937cead62f885726acf33919125d80937c6
zuimoliyan <2019445028@qq.com>
2025-07-31 09:50:49 +0800

    【C端】首页布局效果实现

--- pzH5/src/api/index.js
+++ pzH5/src/api/index.js
@@ -3,7 +3,12 @@ import request from "../utils/request";
 //与后台管理使用不一样的方法：后台管理是暴露每一个接口
 
 export default {
+    //获取登录接口数据
     login(data) {
-        return request.post('/login',data) 
+        return request.post('/login', data)
+    },
+    //获取主页banner的数据
+    index() {
+        return request.get('Index/index')
     }
 }
--- pzH5/src/main.js
+++ pzH5/src/main.js
@@ -3,15 +3,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router";
 import api from "./api";
-//引入消息通知库
-import { Notify } from 'vant';
 
 const app = createApp(App)
 
 //在实例上绑定属性
 app.config.globalProperties.$api = api
-
-app.use(Notify);
 
 //路由挂载
 app.use(router)
